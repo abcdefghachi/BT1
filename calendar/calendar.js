870aa4b86cbfbb256233396a46f91aa2fbcf1edb
abcdefghachi <nguyenhachi793@gmail.com>
2024-02-22 23:53:09 +0700

    fix bt2

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,161 +1,181 @@
-const currentDate = document.getElementById("current-date");
-const day = document.querySelector(".days");
-const nextIcon = document.querySelectorAll(".icons span");
-const todayButton = document.getElementById("today");
-const currentMonthSelect = document.getElementById("current-month-select");
-const currentYearSelect = document.getElementById("current-year-select");
+const currentDate = new Date();
 
-let date = new Date();
-let currentMonth = date.getMonth();
-let currentYear = date.getFullYear();
+const monthSelect = document.getElementById("current-month-selector");
+const yearSelect = document.getElementById("current-year-selector");
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-months.forEach((month, index) => {
-  const option = document.createElement("option");
-  option.value = index;
-  option.text = month;
-  currentMonthSelect.add(option);
-});
-
-const startYear = 1900;
-const endYear = 2050;
-
-for (let year = startYear; year <= endYear; year++) {
-  const option = document.createElement("option");
-  option.value = year;
-  const textNode = document.createTextNode(year);
-  option.appendChild(textNode);
-  currentYearSelect.appendChild(option);
-}
-
-currentMonthSelect.value = currentMonth;
-currentYearSelect.value = currentYear;
-
-currentMonthSelect.addEventListener("change", () => {
-  currentMonth = parseInt(currentMonthSelect.value);
-  calendar();
-});
-
-currentYearSelect.addEventListener("change", () => {
-  currentYear = parseInt(currentYearSelect.value);
-  calendar();
-});
+const monthPopup = document.getElementById("month-popup");
+const monthList = document.getElementById("months");
+const yearPopup = document.getElementById("year-popup");
+const yearList = document.getElementById("years");
 
 const calendar = () => {
-  let selectedDate = new Date(currentYear, currentMonth, 1);
-  let lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-  let firstDayOfMonth = selectedDate.getDay();
-  let lastDayOfMonth = new Date(
-    currentYear,
-    currentMonth,
-    lastDateOfMonth
+  currentDate.setDate(1);
+
+  const days = document.querySelector(".days");
+
+  const lastDateOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
+  ).getDate();
+
+  const lastDateOfLastMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayOfMonth = currentDate.getDay();
+
+  const lastDayOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() + 1,
+    0
   ).getDay();
-  let lastDateOfLastMonth = new Date(currentYear, currentMonth, 0).getDate();
+
+  const nextDate = 7 - lastDayOfMonth - 1;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(
+    ".current-date h2"
+  ).innerHTML = `${currentDate.getFullYear()}`;
+
+  document.querySelector(".current-date h1").innerHTML = `${
+    months[currentDate.getMonth()]
+  }`;
+
+  document.querySelector(".current-date p").innerHTML =
+    new Date().toDateString();
+
   let dateItem = "";
 
-  for (let i = firstDayOfMonth; i > 0; i--) {
-    dateItem += `<li class="other-month">${lastDateOfLastMonth - i + 1}</li>`;
+  for (let x = firstDayOfMonth; x > 0; x--) {
+    dateItem += `<li class="prev-date">${lastDateOfLastMonth - x + 1}</li>`;
   }
 
   for (let i = 1; i <= lastDateOfMonth; i++) {
-    let isCurrentDay =
-      i === date.getDate() &&
-      currentMonth === new Date().getMonth() &&
-      currentYear === new Date().getFullYear()
-        ? "current-day"
-        : "";
-    dateItem += `<li class="${isCurrentDay}">${i}</li>`;
+    if (
+      i === new Date().getDate() &&
+      currentDate.getMonth() === new Date().getMonth() &&
+      currentDate.getFullYear() === new Date().getFullYear()
+    ) {
+      dateItem += `<li class="today">${i}</li>`;
+    } else {
+      dateItem += `<li>${i}</li>`;
+    }
   }
 
-  for (let i = 1; i <= 6 - lastDayOfMonth; i++) {
-    dateItem += `<li class="other-month">${i}</li>`;
+  for (let j = 1; j <= nextDate; j++) {
+    dateItem += `<li class="next-date">${j}</li>`;
   }
-
-  currentDate.innerText = `${months[currentMonth]} , ${currentYear}`;
-  day.innerHTML = dateItem;
-
-  const newDate = document.querySelectorAll(".days li");
-
-  newDate.forEach((dayItem) => {
-    dayItem.addEventListener("click", () => {
-      newDate.forEach((item) => {
-        item.classList.remove("current-day");
-      });
-
-      dayItem.classList.add("current-day");
-    });
-  });
+  days.innerHTML = dateItem;
 };
 
-calendar();
+const handleDayClick = (clickedDay) => {
+  const currentDay = new Date().getDate();
+  const currentMonth = new Date().getMonth();
+  const currentYear = new Date().getFullYear();
 
-nextIcon.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    if (icon.className === "previous") {
-      if (currentMonth === 0) {
-        currentMonth = 11;
-        currentYear--;
-      } else {
-        currentMonth--;
-      }
-    } else if (icon.className === "next") {
-      if (currentMonth === 11) {
-        currentMonth = 0;
-        currentYear++;
-      } else {
-        currentMonth++;
-      }
-    }
+  if (
+    clickedDay === currentDay &&
+    currentDate.getMonth() === currentMonth &&
+    currentDate.getFullYear() === currentYear
+  ) {
+    currentDate.setFullYear(currentYear);
+    currentDate.setMonth(currentMonth);
     calendar();
-  });
-});
-
-document.addEventListener("wheel", (event) => {
-  if (event.deltaY < 0) {
-    changeMonth("previous");
-  } else if (event.deltaY > 0) {
-    changeMonth("next");
   }
-});
+};
 
-todayButton.addEventListener("click", () => {
-  const today = new Date();
-  currentMonth = today.getMonth();
-  currentYear = today.getFullYear();
-  currentMonthSelect.value = currentMonth;
-  currentYearSelect.value = currentYear;
+document.querySelector(".current-date p").addEventListener("click", () => {
+  currentDate.setFullYear(new Date().getFullYear());
+  currentDate.setMonth(new Date().getMonth());
   calendar();
 });
 
-function changeMonth(direction) {
-  if (direction === "previous") {
-    if (currentMonth === 0) {
-      currentMonth = 11;
-      currentYear--;
-    } else {
-      currentMonth--;
-    }
-  } else if (direction === "next") {
-    if (currentMonth === 11) {
-      currentMonth = 0;
-      currentYear++;
-    } else {
-      currentMonth++;
-    }
+monthSelect.onclick = function () {
+  monthPopup.style.display = "block";
+};
+
+window.addEventListener("click", function (event) {
+  if (!monthPopup.contains(event.target) && event.target !== monthSelect) {
+    monthPopup.style.display = "none";
   }
-  calendar();
+});
+
+monthList.addEventListener("click", function (e) {
+  if (e.target && e.target.nodeName == "LI") {
+    const selectedMonthIndex = parseInt(e.target.getAttribute("month"));
+    currentDate.setMonth(selectedMonthIndex);
+    calendar();
+    monthPopup.style.display = "none";
+  }
+});
+
+yearSelect.onclick = function () {
+  renderYearList();
+  yearPopup.style.display = "block";
+};
+
+window.addEventListener("click", function (event) {
+  if (!yearPopup.contains(event.target) && event.target !== yearSelect) {
+    yearPopup.style.display = "none";
+  }
+});
+
+yearList.addEventListener("click", function (e) {
+  if (e.target && e.target.nodeName == "LI") {
+    const selectedYear = parseInt(e.target.textContent);
+    currentDate.setFullYear(selectedYear);
+    calendar();
+    yearPopup.style.display = "none";
+  }
+});
+
+function renderYearList() {
+  const currentYear = new Date().getFullYear();
+  const years = [];
+
+  for (let i = currentYear - 100; i <= currentYear + 99; i++) {
+    years.push(`<li>${i}</li>`);
+  }
+
+  yearList.innerHTML = years.join("");
 }
+document.querySelector(".previous").addEventListener("click", () => {
+  currentDate.setMonth(currentDate.getMonth() - 1);
+  calendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  currentDate.setMonth(currentDate.getMonth() + 1);
+  calendar();
+});
+
+document.querySelector(".calendar").addEventListener("wheel", (event) => {
+  if (event.deltaY < 0) {
+    currentDate.setMonth(currentDate.getMonth() - 1);
+  } else {
+    currentDate.setMonth(currentDate.getMonth() + 1);
+  }
+  calendar();
+});
+
+document.getElementById("years").addEventListener("wheel", function (event) {
+  event.stopPropagation();
+});
+calendar();
